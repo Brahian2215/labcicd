4acd8808c246ae96444b1e63e87af837b4988f22
Brahian2215 <brahian.ocampo@udea.edu.co>
2024-10-02 19:20:39 -0500

    prueba

---
+++ src/main/java/com/udea/labCiCd/LabCiCdApplication.java
@@ -0,0 +1,13 @@
+package com.udea.labCiCd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabCiCdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabCiCdApplication.class, args);
+	}
+
+}
